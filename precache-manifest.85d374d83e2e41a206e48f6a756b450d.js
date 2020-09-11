e9168129e9044bb0144e247b91df3c5f0d5db1d7
zhekaasd <zhekaasd13@gmail.com>
2020-09-11 13:04:15 +0400

    Updates

---
+++ precache-manifest.85d374d83e2e41a206e48f6a756b450d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a69b7480302e9ee3d389f2715c5836",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b51b50dcd398d2a75052",
+    "url": "/portfolio/static/css/main.e4fe8ad2.chunk.css"
+  },
+  {
+    "revision": "db1a56617f05955d83d5",
+    "url": "/portfolio/static/js/2.8b5bfc9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8b5bfc9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51b50dcd398d2a75052",
+    "url": "/portfolio/static/js/main.25a01319.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f847b691c28be94950ba2f17aa02b93d",
+    "url": "/portfolio/static/media/burger-king-logo-png--500.f847b691.png"
+  },
+  {
+    "revision": "dd3f909f0c05447a4bbfd8232706ca6d",
+    "url": "/portfolio/static/media/chip_protsessor_mikroshema_145282_800x600.dd3f909f.jpg"
+  },
+  {
+    "revision": "c0cd138271276bb33a87c2d174015455",
+    "url": "/portfolio/static/media/facebook.c0cd1382.svg"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/portfolio/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "6e333b6ba98b3b3263d71f1cbce1f5d6",
+    "url": "/portfolio/static/media/kinopoisk.ru-Logan-29156492222.6e333b6b.jpg"
+  },
+  {
+    "revision": "182d5ef4d173a3cd93897242a6fd56a5",
+    "url": "/portfolio/static/media/twitter.182d5ef4.svg"
+  },
+  {
+    "revision": "8671d88f1a1afbe7b54d41b5a2fbdc15",
+    "url": "/portfolio/static/media/whatsapp (2).8671d88f.svg"
+  },
+  {
+    "revision": "edeaabc1221e4f8678af2d7e095a1ad6",
+    "url": "/portfolio/static/media/znaki_voprosa_3d_formy_112826_800x600.edeaabc1.jpg"
+  }
+]);
